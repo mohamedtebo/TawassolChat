75e579a41a1b73c29a5f0bb3172fbaed3b53eee3
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-06-18 12:38:39 +0300

    Add themeReducer

--- frontend1/src/store/reducers/rootReducer.js
+++ frontend1/src/store/reducers/rootReducer.js
@@ -1,11 +1,14 @@
 import { combineReducers } from "redux";
 import storage from "redux-persist/lib/storage";
+import themeReducer from "./themeReducer";
 
 const persistConfig = {
     key: 'root',
     storage,
     keyPrefix: 'redux-',
 }
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+    theme: themeReducer
+});
 
 export {persistConfig, rootReducer};
