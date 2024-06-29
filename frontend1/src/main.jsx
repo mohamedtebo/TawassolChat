b509dfed7c25f16141eee1790a68427819ece799
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-06-29 11:02:00 +0300

    Add ToastContainer

--- frontend1/src/main.jsx
+++ frontend1/src/main.jsx
@@ -5,12 +5,26 @@ import './index.css'
 import { Provider } from 'react-redux';
 import { persistor, store } from './store/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
       </PersistGate>
     </Provider>
   </React.StrictMode>,
