6dc88bce04e6b1be500d1fddc39c54bed9355250
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-06-29 11:00:51 +0300

    Create VerifyEmail component

---
+++ frontend1/src/components/auth/VerifyEmail.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import OtpInput from 'react-otp-input';
+
+const VerifyEmail = () => {
+    const [otp, setOtp] = useState('');
+    const [errors, setErrors] = useState({});
+
+    const validateValues = () => {
+        let errors = {}
+        if (otp === '') {
+            errors.otp = "Please enter your otp";
+        }
+        return errors;
+    }
+
+    const handleChangeOtp = (e) => {
+        setOtp(e);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors(validateValues());
+        console.log(otp)
+        // dispatch(registerUser({
+        //     firstName: firstName,
+        //     lastName: lastName,
+        //     phone: phone,
+        //     email: email,
+        //     password: password
+        // }));
+    }
+
+    return (
+        <form className="mt-8 space-y-3" onSubmit={handleSubmit}>
+            <div className='flex flex-col gap-3 relative'>
+                <div className='otp-input-container'>
+                    <OtpInput
+                        value={otp}
+                        onChange={handleChangeOtp}
+                        numInputs={6}
+                        placeholder='000000'
+                        // renderSeparator={<span>-</span>}
+                        renderInput={(props) => <input {...props} />}
+                        isInputNum={true}
+                        separator={<span>-</span>}
+                        shouldAutoFocus={true}
+                        inputStyle="otp-input"
+                        containerStyle="otp-container"
+                    />
+                    {errors.otp && <p className='text-red-300 text-[13px] px-[8px]'>{errors.otp}</p>}
+                </div>
+            </div>
+
+            <button
+                onClick={handleSubmit}
+                className="w-full py-2 px-4 text-sm font-medium rounded-md text-textWhaite bg-primary focus:outline-none"
+            >
+                Confirm
+            </button>
+        </form>
+    );
+}
+
+export default VerifyEmail;
