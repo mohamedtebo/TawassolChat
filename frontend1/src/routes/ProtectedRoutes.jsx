676f41167643fe3a87f72493a65470e84139e9b8
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-06-30 12:28:36 +0300

    Add setTimeout

--- frontend1/src/routes/ProtectedRoutes.jsx
+++ frontend1/src/routes/ProtectedRoutes.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectNotLogged = ({ children, isAuthenticated }) => {
-    if (!isAuthenticated) {
+    const [shouldRedirect, setShouldRedirect] = useState(false);
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            const timer = setTimeout(() => {
+                setShouldRedirect(true);
+            }, 500);
+
+            return () => clearTimeout(timer);
+        }
+    }, [isAuthenticated]);
+
+    if (shouldRedirect) {
         return <Navigate to="/auth" replace />;
     }
     return children;
 }
 
 const ProtectLogged = ({ children, isAuthenticated }) => {
-    if (isAuthenticated) {
+    const [shouldRedirect, setShouldRedirect] = useState(false);
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            const timer = setTimeout(() => {
+                setShouldRedirect(true);
+            }, 1000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [isAuthenticated]);
+
+    if (shouldRedirect) {
         return <Navigate to="/" replace />;
     }
     return children;
