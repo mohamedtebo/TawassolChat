304499a4435d95b68b91a0dbe685b6a8835380b0
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-07-06 16:08:52 +0300

    Create resetPasswordUser custom hook

---
+++ frontend1/src/hooks/auth/useNewPassword.js
@@ -0,0 +1,111 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { sendCodeAgainUser } from '../../store/reducers/authReducer';
+
+const useNewPassword = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {user, user_email, loading, error} = useSelector(state => state.auth);
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errors, setErrors] = useState({});
+    const [showshowPass, setShowPass] = useState(false)
+    const [showConfirmPass, setShowConfirmPass] = useState(false)
+
+    const validateValues = () => {
+        let errors = {}
+        if (password === '') {
+            errors.password = "Please enter your password";
+        }
+        if (confirmPassword === '') {
+            errors.confirmPassword = "Please enter your confirmPassword";
+        }
+        return errors;
+    }
+    
+    const handleChangePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleChangeConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+        
+        if (errors.confirmPassword) {
+            setErrors(prevErrors => ({ ...prevErrors, confirmPassword: '' }));
+        }
+    }
+
+    const handleShowPass = () => {
+        setShowPass(!showshowPass)
+    }
+    const handleShowConfirmPass = () => {
+        setShowConfirmPass(!showConfirmPass)
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        const validationErrors = validateValues();
+        setErrors(validationErrors);
+        
+        // Dispatch resetPasswordUser action if no validation errors
+        if (Object.keys(validationErrors).length === 0) {
+            await dispatch(resetPasswordUser({
+                email:user_email,
+                password: password,
+                passwordConfirm: confirmPassword
+            }));
+        }
+    }
+
+    useEffect(() => {
+        if(user) {
+            console.log(user)
+            if(user.message === "Password Reseted Successfully") {
+                toast.success(user.message);
+
+                setTimeout(() => {
+                    setPassword("")
+                    setConfirmPassword("")
+                }, 500)
+                setTimeout(() => {
+                    navigate('/auth/login')
+                }, 1500)
+            }
+        }
+        if(error) {
+            console.log(error)
+            if(error.message) {
+                setErrors({confirmPassword: error.message});
+            }
+        }
+    }, [user, error])
+
+    const notResetPassword = () => {
+        setTimeout(() => {
+            dispatch(sendCodeAgainUser());
+        }, 1000)
+
+        setTimeout(() => {
+            navigate("/auth/login")
+        }, 1500)
+    }
+
+    return [
+        loading,
+        errors,
+        password,
+        handleChangePassword,
+        confirmPassword,
+        handleChangeConfirmPassword,
+        showshowPass,
+        handleShowPass,
+        showConfirmPass,
+        handleShowConfirmPass,
+        handleSubmit,
+        notResetPassword
+    ]
+}
+
+export default useNewPassword;
