bbe0a9692610379fbcefbfdb1a611562a2e17725
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-07-21 12:07:39 +0300

    Create useVerifyEmail custom hook

---
+++ frontend/src/Hooks/Auth/useVerifyEmail.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { VerifyPasswordUser } from "../../Store/Actions/AuthAction";
+import { sendCodeAgainUser } from "../../Store/Reducers/AuthReducer";
+
+const useVerifyEmail = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {status} = useSelector(state => state.Auth);
+    const [otp, setOtp] = useState('');
+    const [errors, setErrors] = useState({});
+
+    const validateValues = () => {
+        let errors = {}
+        if (otp === '') {
+            errors.otp = "Please enter your otp";
+        }
+        return errors;
+    }
+
+    const handleChangeOtp = (e) => {
+        setOtp(e);
+        if (errors.otp) {
+            setErrors(prevErrors => ({ ...prevErrors, otp: '' }));
+        }
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        const validationErrors = validateValues();
+        setErrors(validationErrors);
+
+        if (Object.keys(validationErrors).length === 0) {
+            await dispatch(VerifyPasswordUser({
+                otp: otp
+            }))
+            .unwrap()
+            .then(user => {
+                if(user.message === "The reset code has been verified successfully") {
+                    toast.success(user.message)
+                    setTimeout(() => {
+                        setOtp('')
+                    }, 500)
+                    setTimeout(() => {
+                        navigate('/auth/new-password')
+                    }, 1000)
+                }
+            })
+            .catch(error => {
+                if(error.message === "Reset code is invalid or has expired") {
+                    setErrors({ otp: error.message });
+                }
+            });
+        }
+    }
+    
+    const sendCodeAgain = () => {
+        setTimeout(() => {
+            dispatch(sendCodeAgainUser());
+        }, 1000)
+
+        setTimeout(() => {
+            navigate("/auth/reset-password")
+        }, 1500)
+    }
+
+    return [
+        otp,
+        handleChangeOtp,
+        handleSubmit,
+        sendCodeAgain,
+        status,
+        errors
+    ]
+}
+
+export default useVerifyEmail;
